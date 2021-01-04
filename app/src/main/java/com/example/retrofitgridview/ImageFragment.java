d58b4f4f251564a77ca8b425b5e9b8c4986d12ff
aotalara <andres.otalora.rodriguez@gmail.com>
2021-01-04 12:55:03 +0100

    Updated 04/01.

---
+++ app/src/main/java/com/example/retrofitgridview/ImageFragment.java
@@ -0,0 +1,42 @@
+package com.example.retrofitgridview;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+
+public class ImageFragment extends Fragment {
+    private String imageUrl;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.page_fragment, container, false);
+
+        ImageView image = (ImageView) v.findViewById(R.id.ivCover);
+        Glide.with(getActivity()).load(imageUrl).into(image);
+        return v;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        imageUrl = getArguments().getString("url");
+    }
+
+    public static ImageFragment newInstance(String text) {
+
+        ImageFragment imageFragment = new ImageFragment();
+        Bundle b = new Bundle();
+        b.putString("url", text);
+
+        imageFragment.setArguments(b);
+
+        return imageFragment;
+    }
+}
